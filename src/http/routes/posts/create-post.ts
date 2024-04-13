927d3d04e429354a978e83a548214e296001fe72
ArthurRodrigomarques <ArthurRodrigomarques@users.noreply.github.com>
2024-04-13 16:26:31 -0300

    add

--- src/http/routes/posts/create-post.ts
+++ src/http/routes/posts/create-post.ts
@@ -1,27 +1,45 @@
-import { z } from "zod"
-import { FastifyInstance } from "fastify"
-import { prisma } from "../../../lib/prisma"
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { Server, IncomingMessage, ServerResponse } from "http";
+import { prisma } from "../../../lib/prisma";
+import uploadImage from "../../../lib/firebase";
+import { z } from "zod";
+
+interface RequestFile {
+  originalname: string;
+  mimetype: string;
+  buffer: Buffer;
+  firebaseUrl?: string;
+}
+
+interface MyFastifyRequest extends FastifyRequest<Server, IncomingMessage, ServerResponse> {
+  file?: RequestFile;
+}
 
 export async function createPost(app: FastifyInstance) {
-  app.post("/post", async (request, reply) => {
-    // Definir o objeto de validação com o Zod
+  app.post("/post", async (request: MyFastifyRequest, reply: FastifyReply<ServerResponse>) => {
     const createPostBody = z.object({
       content: z.string(),
-      coverUrl: z.string(),
+      coverUrl: z.string().optional(),
       published: z.boolean(),
       authorId: z.number(),
-    })
+    });
 
     try {
-      const data = createPostBody.parse(request.body)
+      const data = createPostBody.parse(request.body);
 
-      const post = await prisma.post.create({
-        data,
-      })
-  
-      reply.send(post)
+      await uploadImage(request, reply, async () => {
+        if (request.file && request.file.firebaseUrl) {
+          data.coverUrl = request.file.firebaseUrl;
+        }
+
+        const post = await prisma.post.create({
+          data,
+        });
+
+        reply.send(post);
+      });
     } catch (error) {
-      reply.code(400).send({message: "algo deu errado ao fazer o post"})
+      reply.code(400).send({ message: "algo deu errado ao fazer o post" });
     }
-  })
+  });
 }
